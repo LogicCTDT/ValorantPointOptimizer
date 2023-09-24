5a0f1da83fb1e8919282807c42e612c5e9b87749
Ricky <logicirz@gmail.com>
2023-09-24 04:17:19 -0400

    Made the code classists. Have decided to implemt breach and bound algorithm while taking into account 'overpoints'

--- src/Optimizationator.java
+++ src/Optimizationator.java
@@ -1,13 +1,18 @@
+import java.util.ArrayList;
+
 public class Optimizationator {
     // totalVP we want to get to
     // If we go over the total, we want to keep into the account of total points we went over buy and add that
     // To the cost times some factor.
     int totalCost;
     // Items we have placed into the Optimizationator
-    Item[] Items;
+    ArrayList<Item> Items;
 
 
-
+    public Optimizationator(int totalCost) {
+        this.totalCost = totalCost;
+        this.Items = new ArrayList<Item>();
+    }
 
 
 
