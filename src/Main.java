f9a35bd353791b66ef350d2f558d5ed69c6cc01f
Ricky <logicirz@gmail.com>
2023-09-24 00:59:43 -0400

    Made the code classists. Have decided to implemt breach and bound algorithm while taking into account 'overpoints'

--- src/Main.java
+++ src/Main.java
@@ -42,48 +42,27 @@ public class Main {
         amount.add(11000);
     }
     private static List<Integer> computeBest(int n) {
-        // Always pick from the larger VP category, basically do the highest ammount, then if that doesn't work, do the second highest,etc
+        // Always pick from the larger VP category, basically do the highest ammount, then if that doesn't work, do the
+        // second highest,etc
         // TODO: FIX THIS SHIT, IT LITERALLY DOESN"T WORK, FIGURE OUT W HY YOU SUCK AT PROGRAMMING ADWAOKDAOSK
 
-        int i = 5;
-        double money = 0;
-        int total = n;
-        List<Integer> res = new ArrayList<Integer>(Collections.nCopies(6, 0));
-        while (i >= 0) {
-            int curr = amount.get(i);
-            if (total - curr >= 0) {
-                total = total - curr;
-                money = money + VP_money.get(curr);
-                res.set(i, res.get(i) + 1);
-            }
-            else if (i == 0 && total - curr> = 0) {
-            else {
-                i--;
-            }
-        }
-        return res;
+
+        // Okay, so we will be using the Breach an d Bound Algorithm, using the Greedy Heuristic to bind the upper bound
+        // and relaxation for lower bound. These two are related to knapsack problem.
+
+
+        // This function will be the dfs elements. The idea is visit nodes in bfs style.
+
+        best_value = 0;
+
+
+
+
+
+
 
     }
 
-    private static double computeBestmoney(int n) {
-        // Always pick from the larger VP category, basically do the highest ammount, then if that doesn't work, do the second highest,etc
-        int i = 5;
-        double money = 0;
-        List<Integer> res = new ArrayList<Integer>(Collections.nCopies(6, 0));
-        while (i >= 0) {
-            int curr = amount.get(i);
-            if (n - curr >= 0) {
-                n = n - curr;
-                money = money + VP_money.get(curr);
-                res.set(i, res.get(i) + 1);
-            }
-            else {
-                i--;
-            }
-        }
-        return money;
-
-    }
 
     private static void print(List<Integer> numlist) {
 
